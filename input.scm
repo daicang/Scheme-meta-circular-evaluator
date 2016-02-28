4c3ea6efc55cf10008ab3340e8023788c4418c01
daicang <daicang.cn@gmail.com>
2016-02-28 17:24:02 +0800

    Add  test for recursive function defination and let*.

--- input.scm
+++ input.scm
@@ -1,7 +1,15 @@
 ;; Use '(load "input.scm")' to execute this file.
 
 
-;; Test letrec
+;; let*
+(define (f x)
+  (let* ((a x)
+	 (b (+ a 2)))
+    (+ a b)))
+
+(f 1)
+
+;; letrec
 (define (f x)
   (letrec ((even?
 	    (lambda (n)
@@ -15,11 +23,11 @@
 		  (even? (- n 1))))))
     (even? x)))
 
+(display "Test letrec")
 (f 5)
 (f 1024)
 
-
-;; Test named-let
+;; named-let
 (define (fib n)
   (let iter ((a 1)
 	     (b 0)
@@ -29,5 +37,28 @@
 	(iter (+ a b) a (- count 1)))))
 
 ;; 0 1 1 2 3 5 8
+(display "Test named-let")
 (fib 5)
 (fib 6)
+
+
+;; Recursive function defination
+(define (factorial n)
+  (if (= n 0)
+      1
+      (* n (factorial (- n 1)))))
+
+(display "Test recursive function")
+(factorial 4)
+(factorial 5)
+
+((lambda (f n)
+  (if (= n 0)
+      1
+      (* n (f f (- n 1)))))
+ '(lambda (f n)
+   (if (= n 0)
+       1
+       (* n (f f (- n 1)))))
+ 5)
+    
